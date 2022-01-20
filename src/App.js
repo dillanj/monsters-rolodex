a7f0a43c703d67dd28c22c7d2a81f6b9c1cde45f
dillanj <dillan.johnson1@dmail.dixie.edu>
2022-01-20 12:40:03 -0700

    project complete, just need to deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import { Component } from 'react';
 
-import { CardList } from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
+
+
+// CLASS
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then( users => this.setState({ monsters: users }) )
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value }) 
+  }
+
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='Search Monsters'
+          handleChange={ this.handleChange }
+        />
+        <CardList monsters={filteredMonsters}> </CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
 
 // function App() {
 //   return (
@@ -24,32 +77,3 @@ import './App.css';
 //     </div>
 //   );
 // }
-
-// CLASS
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      monsters: []
-    };
-  }
-
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then( users => this.setState({ monsters: users }) )
-  }
-
-  render(){
-    return (
-      <div className="App">
-        <CardList monsters={this.state.monsters}> 
-          
-         </CardList>
-        
-      </div>
-    );
-  }
-}
-
-export default App;
